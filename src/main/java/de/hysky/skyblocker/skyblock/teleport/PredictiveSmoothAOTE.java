13e38e055469466d29a76e742e786f1d2a5f2c7f
olim <bobq4582@gmail.com>
2025-07-01 15:09:44 +0100

    account for camara moving after teleport

--- src/main/java/de/hysky/skyblocker/skyblock/teleport/PredictiveSmoothAOTE.java
+++ src/main/java/de/hysky/skyblocker/skyblock/teleport/PredictiveSmoothAOTE.java
@@ -1,4 +1,4 @@
-package de.hysky.skyblocker.skyblock.Teleport;
+package de.hysky.skyblocker.skyblock.teleport;
 
 import de.hysky.skyblocker.annotations.Init;
 import de.hysky.skyblocker.config.SkyblockerConfigManager;
